98560631d9b320a52fbc22165fefbc6c33710bee
hoofmen <hoofmen@gmail.com>
2018-10-09 21:56:44 -0700

    routes working and some button funct

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header'
 import GCardList from './components/GCardList'
+import Home from './components/Home';
+import AddCard from './components/AddCard';
+import Settings from './components/Settings';
 import AddCardButton from './components/AddCardButton'
 import Menu from './components/Menu';
 
@@ -9,9 +13,7 @@ class App extends Component {
   constructor (props){ 
     super(props);
     this.state = {
-      drawerMenuOpen: false,
-      views: ['HOME', 'GIFT_CARDS_LIST', 'ADD_NEW_CARD', 'PROFILE', 'SETTINGS'],
-      currentView: 'HOME',
+      drawerMenuOpen: false      
     }
     this.toggleMenu = this.toggleMenu.bind(this);
   }
@@ -22,12 +24,17 @@ class App extends Component {
 
   render() {    
     return (
-      <div>
-        <Header toggleMenu={this.toggleMenu}/>
-        <Menu openMenu={this.state.drawerMenuOpen} toggleMenu={this.toggleMenu}/>
-        <GCardList/>
-        <AddCardButton/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header toggleMenu={this.toggleMenu}/>
+          <Menu openMenu={this.state.drawerMenuOpen} toggleMenu={this.toggleMenu}/>
+            <Route path="/" component={Home} exact />
+            <Route path="/show_cards" component={GCardList} />
+            <Route path="/add_card" component={AddCard} />
+            <Route path="/settings" component={Settings} />
+          <AddCardButton/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
